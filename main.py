aa1263eb6042a39ea9b073888758feb832eb86fa
Awopetu Feyisayo <32976424+feyibosslady@users.noreply.github.com>
2024-06-28 16:01:03 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,84 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import graphviz
+import math
+
+class Value:
+
+    def __init__(self, data, _children=(), _op='', label=''):
+        self.data = data
+        self.grad = 0
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+
+    def __repr__(self):
+        return f"Value(data={self.data})"
+
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self, other), '+')
+        return out
+
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), '*')
+        return out
+
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+        out = Value(t, (self, ), 'tanh')
+        return out
+
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = Value(10.0, label='c')
+e = a*b; e.label = 'e'
+d = a*b + c; d.label = 'd'
+f = Value(-2.0, label = 'f')
+L= d * f; L.label = 'L'
+
+from graphviz import Digraph
+
+def trace(root):
+    nodes, edges = set(), set()
+
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child,v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+def draw_dot(root):
+    dot = Digraph(format = 'svg',graph_attr = {'rankdir':'LR'})
+
+    nodes,edges = trace(root)
+    for n in nodes:
+        uid = str(id(n))
+        dot.node(name = uid, label = "{ %s | data %.4f| grad %.4f}" % (n.label, n.data,n.grad), shape='record')
+        if n._op:
+            dot.node(name = uid + n._op, label = n._op)
+            dot.edge(uid + n._op, uid)
+
+        for n1,n2 in edges:
+            dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+
+        return dot
+
+draw_dot(L)
+
+x1 = Value(2.0, label='x1')
+x2 = Value(0.0, label='x2')
+w1 = Value(-3.0, label='w1')
+w2 = Value(1.0, label='w2')
+b = Value(8.0, label='b')
+x1w1 = x1*w1; x1w1.label = 'x1*w1'
+x2w2 = x2*w2; x2w2.label = 'x2*w2'
+x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1*w1 + x2*w2'
+n = x1w1x2w2 + b; n.label = 'n'
+o = n.tanh()
+draw_dot(o)
